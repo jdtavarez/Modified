b1ac81a5d5efb5a7af64ea83c97b7a4cbc85c946
jdtavarez <juliodtavarez@gmail.com>
2022-01-04 19:17:13 -0500

    dropdown logout

--- frontend/components/splash/navbar/navbar.jsx
+++ frontend/components/splash/navbar/navbar.jsx
@@ -4,17 +4,37 @@ import { Link } from "react-router-dom";
 export default class NavBar extends React.Component {
     constructor(props) {
         super(props)
+        this.handleNavClick = this.handleNavClick.bind(this)
+    }
+
+    handleNavClick() {
+        const arrow = document.getElementById("arrow");
+        if (arrow.className === 'fas fa-angle-up') {
+            arrow.className = 'fas fa-angle-down'
+            const container = document.getElementsByClassName("nav-dropdown-content")[0];
+            container.classList.remove("display-items")
+        } else {  
+            arrow.className = 'fas fa-angle-up'; 
+            const container = document.getElementsByClassName("nav-dropdown-content")[0];
+            container.classList.add("display-items")
+        }
     }
 
     render() {
         const nav_display = this.props.currentUser ? (
             <div className="splash-links">
                 <p className="divider">|</p>
-                <div className="profile-container">
-                    <i className="far fa-user"></i>
-                    <p>Profile</p>
+                <div className="nav-dropdown">
+                    <div className="nav-btn" onClick={this.handleNavClick}>
+                        <i className="far fa-user"></i>
+                        <p>Profile</p>
+                        <i className="fas fa-angle-down" id="arrow"></i>
+                    </div>
+                    <div className="nav-dropdown-content">
+                        <a onClick={this.props.logout}>Log out</a>
+                    </div>
                 </div>
-                <button onClick={this.props.logout}>logout</button>
+                
             </div>
         ) : (
             <div className="splash-links">
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,5 +2,6 @@ import React from 'react'
 
 export default () => (
     <div id="splash">
+        <p id="webplayer-header">Jump back in</p>
     </div>
 )
