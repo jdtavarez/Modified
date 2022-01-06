930761b3fc294c04f052ebb32fc8260b177d5f22
jdtavarez <juliodtavarez@gmail.com>
2022-01-06 18:56:42 -0500

    cosmetic edits

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import { AuthRoute } from '../util/route_util';
 import NavBarContainer from './splash/navbar/navbar_container'
-import Splash from './splash/splash'
+import SplashContainer from './splash/splash_container'
 import LoginFormContainer from './splash/session/login_form_container';
 import SignUpFormContainer from './splash/session/signup_form_container'
 
@@ -10,7 +10,7 @@ import SignUpFormContainer from './splash/session/signup_form_container'
 const App = () => (
     <div id="app-container">
         <Route exact path="/" component={NavBarContainer} />
-        <Route exact path="/" component={Splash} />
+        <Route exact path="/" component={SplashContainer} />
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignUpFormContainer} />
     </div>
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default () => (
-    <div className='splash'>
-        <main className="webplayer-header">
-            <div className='container'>
-                <h3>MODIFIED FREE</h3>
-                <h1>Listening is everything</h1>
-                <h2>Millions of songs and podcasts. No credit card needed.</h2>
-                <Link id="weblink" to="/web">GET MODIFIED FREE</Link>
-            </div>
-        </main>
-        <footer>
+ class Splash extends React.Component {
+     constructor(props) {
+         super(props)
+     }
 
-        </footer>
-    </div>
-)
+    render() {
+        
+        const message = this.props.currentUser ? (<Link id="weblink" to="/web">GO TO WEB PLAYER</Link>) : (<Link id="weblink" to="/signup">GET MODIFIED FREE</Link>)
+
+        return (
+        <div className='splash'>
+            <main className="webplayer-header">
+                <div className='container'>
+                    <h3>MODIFIED FREE</h3>
+                    <h1>Listening is everything</h1>
+                    <h2>Millions of songs and podcasts. No credit card needed.</h2>
+                    {message}
+                </div>
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+        )
+    }
+}
+
+export default Splash
