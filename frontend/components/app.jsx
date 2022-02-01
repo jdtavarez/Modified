85ba8d3558bfa2051c372845ba0add5d21911941
jdtavarez <juliodtavarez@gmail.com>
2022-02-01 16:08:03 -0500

    add image

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SplashPage from './splash/splash_page'
 import WebPlayer from './webplayer/webplayer'
 import LoginFormContainer from './splash/session/login_form_container';
@@ -12,7 +12,7 @@ const App = () => (
         <Route exact path="/" component={SplashPage} />
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignUpFormContainer} />
-        <Route path="/web" component={WebPlayer}/>
+        <ProtectedRoute path="/web" component={WebPlayer}/>
     </div>
 );
 
